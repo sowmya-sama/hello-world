4b58ddbaf7fc4a5aa2d4fd2a02577b25156fc756
sowmya-sama <ssama@luc.edu>
2012-09-11 00:04:39 -0500

    added counter button and logic

--- src/com/comp412/android/HelloWorld.java
+++ src/com/comp412/android/HelloWorld.java
@@ -1,13 +1,24 @@
 package com.comp412.android;
 
 import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class HelloWorld extends Activity {
     /** Called when the activity is first created. */
+	private int mCount = 0;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final Button countButton = (Button) findViewById(R.id.CountButton);         
+        countButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                mCount++;
+                countButton.setText("Count: " + mCount);
+            }
+        });  
     }
 }
